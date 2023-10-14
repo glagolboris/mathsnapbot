7109611610686d6f970ff7f06bf52ffd53fa9f05
MrHappyCloud <mrhappycloud1@gmail.com>
2023-10-14 15:19:15 +0300

    Add `database.py` and `requirements.txt`

---
+++ database.py
@@ -0,0 +1,24 @@
+import aiosqlite as sql
+
+
+class Database:
+    async def init_table() -> None:
+        async with sql.connect("database.sqlite") as db:
+            await db.execute(
+                """CREATE TABLE IF NOT EXISTS Data (
+                    ChatID INTEGER NOT NULL,
+                    History STRING
+                )
+                """
+            )
+            await db.commit()
+
+    async def get_history(chat_id: int) -> list:
+        async with sql.connect("database.sqlite") as db:
+            cur = await db.execute("SELECT History FROM Data WHERE ChatID = ?", (chat_id,))
+            return await cur.fetchall()
+
+    async def insert(*args, **kwargs):
+        async with sql.connect("database.sqlite") as db:
+            await db.execute(*args, **kwargs)
+            await db.commit()
